7b0640c441270b2b776fcb1ad6c50a986014e477
Yara Amr-Allah <yara.amrallah.94@gmail.com>
2021-01-31 08:26:16 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    flags: isDocker ? ['--no-sandbox'] : [],
+    flags: ['--no-sandbox'],
    // singleRun: false,
     restartOnFileChange: true
   });
